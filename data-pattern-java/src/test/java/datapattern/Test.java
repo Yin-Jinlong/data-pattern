05c4f88325961178e23c2cbf6170e8bc82bcf2a4
yjl <yjl_1567@qq.com>
2023-01-07 22:56:21 +0800

    测试

---
+++ data-pattern-java/src/test/java/datapattern/Test.java
@@ -0,0 +1,18 @@
+package datapattern;
+
+import com.github.yinjinlong.datapattern.DataPattern;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		//DataPattern.loadDataFiles();
+		System.out.println("手机号");
+		System.out.println(DataPattern.isPhonePattern("18712345678"));
+		System.out.println(DataPattern.isPhonePattern("19412345678"));
+		System.out.println(DataPattern.isPhonePattern("+86 18712345678"));
+		System.out.println("邮箱");
+		System.out.println(DataPattern.isEmail("aaa@aaa.cn"));
+		//身份证号隐私，已经测试通过
+	}
+	
+}
